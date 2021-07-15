f600529134992b30b991a196b20f8d70259a74d3
GYBYOCONER <87486861+GYBYOCONER@users.noreply.github.com>
2021-07-15 10:11:07 -0500

    Create editor.java

---
+++ editor.java
@@ -0,0 +1,5 @@
+javac HolaMundo.java.
+java HolaMundo.
+public class HolaMundo { public static void main(String[] args) { System.out.println("¡ Hola mundo!"); } }
+(String[] args)
+System. out. println("¡Hola mundo!");
